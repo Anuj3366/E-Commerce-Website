69964cdaa1ec2c19447719bac7a0b424f835a435
Anuj Garg <anujgarg3366@gmail.com>
2023-09-01 20:16:54 +0530

    made home page

--- script.js
+++ script.js
@@ -1,26 +1,143 @@
-// Sample products data
+const item = document.querySelectorAll(".Item");
+const wrapper = document.querySelector(".sliderWrapper");
+
 const products = [
-    { name: 'Product 1', price: 19.99 },
-    { name: 'Product 2', price: 29.99 },
-    // Add more products here
+{
+    id : 1,
+    title: "Air-Force",
+    price: 12000 ,
+    colors : [
+    {
+        code:"black",
+        img: "/images/air.png",
+    },
+    {
+        code:"blue",
+        img: "/images/air2.png",
+    },
+    ],
+},
+{
+    id : 2,
+    title: "Air-Jordan",
+    price: 18000 ,
+    colors : [
+    {
+        code:"gray",
+        img: "/images/jordan.png",
+    },
+    {
+        code:"green",
+        img: "/images/jordan2.png",
+    },
+    ],
+},
+
+{
+    id : 3,
+    title: "blazer",
+    price: 10000 ,
+    colors : [
+    {
+        code:"rgb(255, 221, 221)",
+        img: "/images/blazer.png",
+    },
+    {
+        code:"green",
+        img: "/images/blazer2.png",
+    },
+    ],
+},
+
+{
+    id : 4,
+    title: "Crater",
+    price: 11000 ,
+    colors : [
+    {
+        code:"black",
+        img: "/images/crater.png",
+    },
+    {
+        code:"rgb(188, 255, 4)",
+        img: "/images/crater2.png",
+    },
+    ],
+},
+
+
+{
+    id : 5,
+    title: "Hippie",
+    price: 15000 ,
+    colors : [
+    {
+        code:"pink",
+        img:  "/images/hippie.png",
+    },
+    {
+        code:"rgb(62, 56, 56)",
+        img: "/images/hippie2.png",
+    },
+    ],
+},
 ];
 
-// Function to display products
-function displayProducts() {
-    const productsSection = document.querySelector('.products');
-    
-    products.forEach(product => {
-        const productCard = document.createElement('div');
-        productCard.className = 'product-card';
-        productCard.innerHTML = `
-            <h3>${product.name}</h3>
-            <p>Price: $${product.price}</p>
-            <button>Add to Cart</button>
-        `;
-        
-        productsSection.appendChild(productCard);
-    });
-}
+const productTitle = document.querySelector(".productTitle");
+const productPrice = document.querySelector(".productPrice");
+const productColor = document.querySelectorAll(".color");
+const productImg = document.querySelector(".productImg");
+const productSize = document.querySelectorAll(".size");
 
-// Call the function to display products
-displayProducts();
+
+let choosenProduct = products[0];
+
+
+item.forEach((items, index) => {
+    items.addEventListener("click", () => {
+        // choose slide
+        wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+        // get details of that slide
+        choosenProduct = products[index];
+
+        // change the slide name, color and price
+        productTitle.textContent = choosenProduct.title;
+        productPrice.textContent = "₹" +choosenProduct.price;
+        productImg.src = choosenProduct.colors[0].img;
+
+        productColor.forEach((color,index) => {
+            color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
+    });
+});
+
+productColor.forEach((color, index) => {
+    color.addEventListener("click", ()=> {
+        productImg.src = choosenProduct.colors[index].img;
+    });
+});
+
+productSize.forEach((size, index) => {
+    size.addEventListener("click", ()=> {
+        productSize.forEach((size) => {
+            size.style.backgroundColor = "white";
+             size.style.color = "black";
+        });
+        size.style.backgroundColor = "black";
+        size.style.color = "white";
+    });
+    
+});
+
+const buyButton = document.querySelector(".productButton");
+const close = document.querySelector(".close");
+const payment = document.querySelector(".payment");
+
+buyButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
+});
